78979d020fe75b675866412585f36dd9c75afeb7
Sam Der <samder2468@gmail.com>
2020-08-23 19:54:54 -0700

    Updates

---
+++ precache-manifest.a4fb915117fcaf7376ae086162d011ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504a67362a8528588e88b2d7a1f6aaaf",
+    "url": "/themarketplace/index.html"
+  },
+  {
+    "revision": "1e8a7b7ca77b834e0a52",
+    "url": "/themarketplace/static/css/main.bce2b9b5.chunk.css"
+  },
+  {
+    "revision": "cf86e115d501a4388c3b",
+    "url": "/themarketplace/static/js/2.25c34857.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/themarketplace/static/js/2.25c34857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8a7b7ca77b834e0a52",
+    "url": "/themarketplace/static/js/main.a5ad4151.chunk.js"
+  },
+  {
+    "revision": "0cb763d4d26c20591f0e",
+    "url": "/themarketplace/static/js/runtime-main.8dc200a1.js"
+  }
+]);
